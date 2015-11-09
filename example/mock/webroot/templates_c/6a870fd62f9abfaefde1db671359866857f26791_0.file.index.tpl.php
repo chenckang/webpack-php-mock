ed9e16f72d73bab2b079ee4d3754ef3cd39cccfe
Kang Chen <chenckang@gmail.com>
2015-11-09 15:31:07 +0800

    fix bug

--- example/mock/webroot/templates_c/6a870fd62f9abfaefde1db671359866857f26791_0.file.index.tpl.php
+++ example/mock/webroot/templates_c/6a870fd62f9abfaefde1db671359866857f26791_0.file.index.tpl.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.27, created on 2015-11-09 15:17:38
+<?php /* Smarty version 3.1.27, created on 2015-11-09 15:22:49
          compiled from "/Users/ck/Documents/webpack-php-mock/example/mock/webroot/templates/index.tpl" */ ?>
 <?php
-/*%%SmartyHeaderCode:75031908856404892114ce2_12193021%%*/
+/*%%SmartyHeaderCode:453357438564049c99b7062_14528242%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,20 +9,20 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '6a870fd62f9abfaefde1db671359866857f26791' => 
     array (
       0 => '/Users/ck/Documents/webpack-php-mock/example/mock/webroot/templates/index.tpl',
-      1 => 1447053454,
+      1 => 1447053765,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '75031908856404892114ce2_12193021',
+  'nocache_hash' => '453357438564049c99b7062_14528242',
   'has_nocache_code' => false,
   'version' => '3.1.27',
-  'unifunc' => 'content_5640489212a9d6_61411520',
+  'unifunc' => 'content_564049c99ce137_79695630',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_5640489212a9d6_61411520')) {
-function content_5640489212a9d6_61411520 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_564049c99ce137_79695630')) {
+function content_564049c99ce137_79695630 ($_smarty_tpl) {
 
-$_smarty_tpl->properties['nocache_hash'] = '75031908856404892114ce2_12193021';
+$_smarty_tpl->properties['nocache_hash'] = '453357438564049c99b7062_14528242';
 ?>
 
 <!DOCTYPE html>
